a5f1a15863b4a9b39b2e08df1aaee0ed6d5736ae
Henri Casanova <henric@hawaii.edu>
2024-10-23 18:34:40 -1000

    Fixes

--- run_experiments.py
+++ run_experiments.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
                     mems = []
                     for seed in range(0, num_trials):
 
-                        command = f"docker run -it --rm -w /home/simgrid/build_simgrid_{version}/ -v `pwd`:/home/simgrid simgrid_{version} /usr/bin/time -v ./master_worker_{version} {num_hosts} {num_cores_per_host} {min_core_speed} {max_core_speed} {num_links} {min_bandwidth} {max_bandwidth} {route_length} {num_workunits} {min_computation} {max_computation} {min_data_size} {max_data_size} {seed} --log=root.thresh:critical {energy_plugins[version]}"
+                        command = f"docker run -it --rm --memory 8000m -w /home/simgrid/build_simgrid_{version}/ -v `pwd`:/home/simgrid simgrid_{version} /usr/bin/time -v ./master_worker_{version} {num_hosts} {num_cores_per_host} {min_core_speed} {max_core_speed} {num_links} {min_bandwidth} {max_bandwidth} {route_length} {num_workunits} {min_computation} {max_computation} {min_data_size} {max_data_size} {seed} --log=root.thresh:critical {energy_plugins[version]}"
                         # print(command)
 
                         try:
